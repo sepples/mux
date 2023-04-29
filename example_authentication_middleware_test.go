a9fb87145ac950a24495d12399ae09acf0e2fe84
sepples <sepples@proton.me>
2023-04-29 18:49:36 +0100

    chore: update module paths + misc

--- example_authentication_middleware_test.go
+++ example_authentication_middleware_test.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/sepples/mux"
 )
 
 // Define our struct
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/gorilla/mux
+module github.com/sepples/mux
 
 go 1.12
